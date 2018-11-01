a5b3024800c0e95a137a79ed9979a0abfd7a13f2
Erik Perkins <erik.andrew.perkins@gmail.com>
2018-10-31 17:40:48 -0700

    Enable live SCSS reload

--- lib/bayberry_web/endpoint.ex
+++ lib/bayberry_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule BayberryWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :bayberry,
-    gzip: true,
+    gzip: false,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
