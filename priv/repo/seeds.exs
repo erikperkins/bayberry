fabe2ef9601c859e2c05277a1072922153ccf33e
Erik Perkins <erik.andrew.perkins@gmail.com>
2018-11-02 00:51:42 -0700

    Make database seeds idempotent

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,28 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+require Logger
 alias Bayberry.{Accounts, Blog, Repo}
+alias Bayberry.Accounts.User
 alias Bayberry.Administration.{Visit, Visitor}
-
+alias Bayberry.Blog.{Article, Author}
 
 admin_params = %{
   "name" => "administrator",
-  "username" => "administrator",
   "credential" => %{
     "email" => "admin@datapun.net",
     "password" => "datapunnet"
   }
 }
 
-{:ok, user} = Accounts.create_user(admin_params)
-
 author_params = %{
   "bio" => "admin",
   "role" => "admin",
   "genre" => "admin"
 }
-
-{:ok, author} = Blog.create_author(user, author_params)
 
 article_params = %{
   "title" => "Test",
@@ -48,26 +45,68 @@ article_params = %{
   """
 }
 
-Blog.create_article(author, article_params)
-
 visitor_params = %{
   "ip_address" => "66.249.64.0",
   "latitude" => 37.406000,
   "longitude" => -122.078500
 }
 
-{:ok, visitor} =
-  %Visitor{}
-  |> Visitor.changeset(visitor_params)
-  |> Repo.insert()
-
 visit_params = %{
   "path" => "/",
   "user_agent" => "Googlebot/2.1 (+http://www.google.com/bot.html)"
 }
 
-{:ok, visit} =
-  %Visit{visitor: visitor}
-  |> Visit.changeset(visit_params)
-  |> Ecto.Changeset.cast_assoc(:visitor, with: &Visitor.changeset/2)
-  |> Repo.insert()
+user =
+  case Repo.get_by(User, name: admin_params["name"]) do
+    nil ->
+      {:ok, user} = Accounts.create_user(admin_params)
+      user
+
+    user ->
+      Logger.warn("User already created")
+      user
+  end
+
+author =
+  case Repo.get_by(Author, bio: author_params["bio"]) do
+    nil ->
+      {:ok, author} = Blog.create_author(user, author_params)
+      author
+
+    author ->
+      Logger.warn("Author already created")
+      author
+  end
+
+case Repo.get_by(Article, title: article_params["title"]) do
+  nil ->
+    {:ok, article} = Blog.create_article(author, article_params)
+    article
+
+  article ->
+     Logger.warn("Article already created")
+    article
+end
+
+visitor =
+  case Repo.get_by(Visitor, ip_address: visitor_params["ip_address"]) do
+    nil ->
+      %Visitor{}
+      |> Visitor.changeset(visitor_params)
+      |> Repo.insert!()
+
+    visitor ->
+      Logger.warn("Visitor already created")
+      visitor
+  end
+
+case Repo.get_by(Visit, user_agent: visit_params["user_agent"]) do
+  nil ->
+    %Visit{}
+    |> Visit.changeset(visit_params)
+    |> Ecto.Changeset.put_change(:visitor_id, visitor.id)
+    |> Repo.insert!()
+
+  _ ->
+    Logger.warn("Visit already created")
+end
