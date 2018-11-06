5a05769d2c4526dfeb2194db8bef4a05895c2d97
Erik Perkins <erik.andrew.perkins@gmail.com>
2018-11-05 21:06:02 -0800

    Add digit tooltips

--- assets/js/main/digits.js
+++ assets/js/main/digits.js
@@ -19,8 +19,9 @@ export var Digits = {
 
     function replaceDigit(payload) {
       let nodes = d3.selectAll(".digit").nodes()
+      let tooltip = d3.select(".digit-tooltip")
 
-      let j =  Math.floor(d3.randomUniform(0, nodes.length)())
+      let j = Math.floor(d3.randomUniform(0, nodes.length)())
       i = (i == j) ? (i + 1) % 10 : j
       var node = d3.select(nodes[i])
 
@@ -28,24 +29,32 @@ export var Digits = {
       let fadein = d3.transition().duration(250)
 
       node.transition(fadeout).style("opacity", 0)
-        .on("end", () => {
+        .on("end", function() {
           let url = `data:image/png;base64,${payload.image}`
           node.attr("xlink:href", () => url)
-            .html(() => `<title>${payload.classification}</title>`)
 
           node.transition(fadein).style("opacity", 1)
+
+          $(this).tooltip("hide")
+            .attr("data-original-title", payload.classification)
+            .tooltip("setContent")
+
+          $(this).filter(":hover").tooltip("show")
         })
     }
 
     function cloud(digits) {
+      $(function() { $("[data-toggle='tooltip']").tooltip() })
+
       let
         width = 200,
-        height = 200
+        height = 200,
+        imageWidth = 28,
+        imageHeight = 28
 
       let svg = d3.select("#digitcloud")
         .attr("width", width)
         .attr("height", height)
-        .style("fill", "red")
 
       let group = svg.append("g")
         .attr("transform", `translate(${width/2},${height/2})`)
@@ -53,24 +62,22 @@ export var Digits = {
       let bodies = d3.forceSimulation(digits)
         .force("charge", d3.forceManyBody().strength(-10/digits.length))
         .force("collide", d3.forceCollide(14 * Math.sqrt(2)))
-        .force("center", d3.forceCenter(-14, -14))
-        .on("tick", update)
+        .force("center", d3.forceCenter(-(imageWidth/2), -(imageHeight/2)))
+        .on("tick", move)
 
-      function update() {
-        let selection = group.selectAll(".digit").data(digits)
+      let images = group.selectAll(".digit")
+        .data(digits).enter()
+        .append("image")
+        .attr("class", "digit")
+        .attr("width", imageWidth)
+        .attr("height", imageHeight)
+        .attr("xlink:href", d => `data:image/png;base64,${d.image}`)
+        .attr("data-toggle", "tooltip")
+        .attr("data-placement", "left")
+        .attr("title", d => d.classification)
 
-        selection.enter()
-          .append("image")
-          .attr("class", "digit")
-          .attr("xlink:href", d => `data:image/png;base64,${d.image}`)
-          .attr("width", 28)
-          .attr("height", 28)
-          .html(d => `<title>${d.classification}</title>`)
-          .merge(selection)
-          .attr("x", d => d.x)
-          .attr("y", d => d.y)
-
-        selection.exit().remove()
+      function move() {
+        images.attr("x", d => d.x).attr("y", d => d.y)
       }
     }
   }
