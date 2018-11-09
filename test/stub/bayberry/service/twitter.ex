39c1dde0606f62ae8e669fa074b67c708142ed15
Erik Perkins <erik.andrew.perkins@gmail.com>
2018-11-08 16:55:35 -0800

    Adjust tweet styling

--- test/stub/bayberry/service/twitter.ex
+++ test/stub/bayberry/service/twitter.ex
@@ -5,7 +5,12 @@ defmodule Stub.Bayberry.Service.Twitter do
   @rabbitmq get_env(:bayberry, Bayberry.Service)[:rabbitmq]
 
   def broadcast(%{"text" => text}) do
-    Endpoint.broadcast("twitter:stream", "tweet", %{body: text} || %{})
+    text
+    |> hyperlink()
+    |> hashtag()
+    |> atmention()
+    |> (&Regex.replace(~r/\n|\r/, &1, "\n")).()
+    |> (&Endpoint.broadcast("twitter:stream", "tweet", %{body: &1} || %{})).()
   end
 
   def produce(%{channel: channel} = state) do
@@ -16,12 +21,30 @@ defmodule Stub.Bayberry.Service.Twitter do
     produce(state)
   end
 
+  defp hyperlink(text) do
+    link_regex = ~r/(https?:\/\/([-\w\.]+)+(:\d+)?(\/([\w\/_\.]*(\?\S+)?)?)?)/
+    link = "<a href='\\1' target='_blank'>\\1</a>"
+    Regex.replace(link_regex, text, link)
+  end
+
+  defp hashtag(text) do
+    hashtag_regex = ~r/\#([\w]+)?/
+    hashtag = ~s[<a href="https://twitter.com/hashtag/\\1?src=hash" target="_blank">#\\1</a>]
+    Regex.replace(hashtag_regex, text, hashtag)
+  end
+
+  defp atmention(text) do
+    atmention_regex = ~r/\@([\w]+)?/
+    atmention = ~s[<a href="https://twitter.com/\\1" target="_blank">@\\1</a>]
+    Regex.replace(atmention_regex, text, atmention)
+  end
+
   defp stub() do
     case Enum.random([0, 1, 2, 3]) do
       0 ->
         """
-        My existence is amazing, and I want to study algebra.
-        Ridiculously derailed candy, please. #happytag #randomtweet
+        @Wikipedia My existence is amazing, and I want to study algebra.
+        https://wikipedia.org #wikipedia
         """
       1 ->
         """
@@ -41,6 +64,8 @@ defmodule Stub.Bayberry.Service.Twitter do
         A bit of hand crafted lies, again. #midnightbling #randomtweet
         My cooking is an institution, and I want to be heard.
         Ridiculously thirsty traffic, man. #bunnywad #randomtweet
+        My intelligence is a joy, and I want to learn everything.
+        The world has strange pets, I say. #yolo2u #randomtweet
         """
     end
   end
